cf282beb0509b87220b51171f062ee1b7c67a6e1
Alexander Adabir <adabiralexander@gmail.com>
2022-11-17 18:39:44 +0300

    add comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 export default function App() {
   return (
     <div className="container mx-auto max-w-[1080px] bg-white rounded-3xl">
+      {/* Aside */}
       <aside className="bg-black/50 w-full h-full absolute top-0 left-0 z-10">
+        {/* Aside Container */}
         <div className="bg-white w-[400px] h-full p-8 absolute right-0 top-0 flex flex-col justify-between">
+          {/* Asider Header */}
           <div>
             <h2 className="text-2xl font-bold mb-7">Корзина</h2>
 
+            {/* BuyItemsContainer */}
             <div className="flex flex-col gap-5">
               {/* BuyItem */}
               <div className="flex items-center gap-5 p-5 border rounded-2xl">
@@ -49,8 +53,11 @@ export default function App() {
               </div>
               {/* ./BuyItem */}
             </div>
+            {/* ./BuyItemsContainer */}
           </div>
+          {/* ./Aside Header */}
 
+          {/* Aside Footer */}
           <div className="flex flex-col gap-5">
             <div className="flex justify-between items-baseline">
               <p>Итого:</p>
@@ -66,11 +73,16 @@ export default function App() {
               Оформить заказ
             </button>
           </div>
+          {/* ./Aside Footer */}
         </div>
+        {/* ./Aside Container */}
       </aside>
+      {/* ./Aside */}
 
       <header className="border-b p-10">
+        {/* Header Container */}
         <div className="flex justify-between">
+          {/* Header Left */}
           <div className="flex items-center gap-3">
             <img
               width={40}
@@ -85,7 +97,9 @@ export default function App() {
               <p className="text-sm opacity-60">Магазин лучших кроссовок</p>
             </div>
           </div>
+          {/* ./Header Left */}
 
+          {/* Header Right */}
           <ul className="flex gap-7 items-center">
             <li className="flex items-center cursor-pointer">
               <img
@@ -103,11 +117,14 @@ export default function App() {
               <img width={20} src="/img/user.svg" alt="Пользователь" />
             </li>
           </ul>
+          {/* ./Header Right */}
         </div>
+        {/* ./Header Container */}
       </header>
 
       <main className="px-14 py-10">
         <section>
+          {/* Section Header */}
           <div className="flex justify-between items-center mb-16">
             <h1 className="font-bold text-3xl">Все кроссовки</h1>
 
@@ -121,7 +138,9 @@ export default function App() {
               />
             </div>
           </div>
+          {/* ./Section Header */}
 
+          {/* Cards Wrapper */}
           <div className="flex justify-between gap-10">
             {/* Card */}
             <div className="max-w-[210px] border border-[#F3F3F3] hover:border-[#F8F8F8] hover:shadow-md hover:-translate-y-2 transition-all duration-300 rounded-3xl py-5 px-7">
@@ -217,6 +236,7 @@ export default function App() {
             </div>
             {/* ./Card */}
           </div>
+          {/* ./Cards Wrapper */}
         </section>
       </main>
     </div>
