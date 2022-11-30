c9ea26eeaf635b56eae650b389179166703d70b2
Alexander Adabir <adabiralexander@gmail.com>
2022-11-30 19:00:35 +0300

    fix shopItem on mobile layout

--- src/components/ShoppingCartItem.jsx
+++ src/components/ShoppingCartItem.jsx
@@ -1,12 +1,12 @@
 export default function ShoppingCartItem(props) {
   const { path, text, price, onRemoveItem } = props
   return (
-    <div className="flex items-center gap-3 rounded-2xl sm:gap-5 sm:border sm:p-5">
+    <div className="flex items-center justify-between  gap-3 rounded-2xl sm:gap-5 sm:border sm:p-5">
       <div
         style={{
           background: `url(${path}) no-repeat center /contain`,
         }}
-        className="h-[70px] w-full max-w-[70px]"
+        className="h-[70px] w-[70px]"
       ></div>
 
       <div className="max-w-[150px] text-sm">
@@ -19,7 +19,7 @@ export default function ShoppingCartItem(props) {
         onClick={() => onRemoveItem(props)}
         className="opacity-50 duration-300 ease-linear hover:opacity-100 "
       >
-        <img src="img/btn-remove.svg" alt="Удалить" />
+        <img src="/img/btn-remove.svg" alt="Удалить" />
       </button>
     </div>
   )
